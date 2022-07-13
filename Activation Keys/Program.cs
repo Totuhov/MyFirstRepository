b5f2bcd52181fb535a910c516d4d749c968852dd
Nikolay Totuhov <nikolaytotuhov@gmail.com>
2022-07-13 22:19:20 +0200

    testGuidHubDesktop
    added some exercises

---
+++ Activation Keys/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+
+namespace Activation_Keys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string activationKey = Console.ReadLine();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "Generate")
+            {
+                string[] commandArray = command.Split(">>>", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string task = commandArray[0];
+
+                if (task == "Contains")
+                {
+                    Contains(activationKey, commandArray);
+                }
+                else if (task == "Flip")
+                {
+                    Flip(commandArray, ref activationKey);
+                }
+                else if (task == "Slice")
+                {
+                    Slice(commandArray, ref activationKey);
+                }
+            }
+            Console.WriteLine($"Your activation key is: {activationKey}");
+        }
+
+
+        static void Contains(string activationKey, string[] commandArray)
+        {
+            string substring = commandArray[1];
+
+            if (activationKey.Contains(substring))
+            {
+                Console.WriteLine($"{activationKey} contains {substring}");
+            }
+            else
+            {
+                Console.WriteLine("Substring not found!");
+            }
+        }
+        static void Flip(string[] commandArray, ref string activationKey)
+        {
+            string subTask = commandArray[1];
+            int startIndex = int.Parse(commandArray[2]);
+            int endIndex = int.Parse(commandArray[3]);
+            string subString;
+            string subStringNew;
+
+            if (subTask == "Upper")
+            {
+                subString = activationKey.Substring(startIndex, endIndex - startIndex);
+                subStringNew = subString.ToUpper();
+                activationKey = activationKey.Replace(subString, subStringNew);
+                Console.WriteLine(activationKey);
+            }
+            else
+            {
+                subString = activationKey.Substring(startIndex, endIndex - startIndex);
+                subStringNew = subString.ToLower();
+                activationKey = activationKey.Replace(subString, subStringNew);
+                Console.WriteLine(activationKey);
+            }
+        }
+        static void Slice(string[] commandArray, ref string activationKey)
+        {
+            int startIndex = int.Parse(commandArray[1]);
+            int endIndex = int.Parse(commandArray[2]);
+
+            activationKey = activationKey.Remove(startIndex, endIndex - startIndex);
+
+            Console.WriteLine(activationKey);
+        }
+    }
+}
---
+++ Emoji Detector/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace Emoji_Detector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(:{2}|\*{2})(?<name>[A-Z]{1}[a-z]{2,})\1";
+            string patternInt = @"[1-9]";
+
+            string input = Console.ReadLine();
+
+            Dictionary<string, int> emojies = new Dictionary<string, int>();
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+            MatchCollection integers = Regex.Matches(input, patternInt);
+
+            BigInteger bigInteger = new BigInteger(1);
+            
+
+            foreach (var x in integers)
+            {
+                string a = x.ToString();
+                bigInteger *= int.Parse(a);
+            }
+            Console.WriteLine($"Cool threshold: {bigInteger}");
+
+            BigInteger score = new BigInteger(0);
+
+            Console.WriteLine($"{matches.Count} emojis found in the text. The cool ones are:");
+
+            foreach (Match match in matches)
+            {
+                string matchString = match.Groups["name"].Value;
+
+                foreach (char ch in matchString)
+                {
+                    score += (int)ch;
+                }
+                if (score >= bigInteger)
+                {
+                    emojies.Add(match.ToString(), (int)score);
+                }                
+                score = 0;
+            }
+            foreach (var emoji in emojies)
+            {
+                Console.WriteLine(emoji.Key);
+            }
+        }
+    }
+}
